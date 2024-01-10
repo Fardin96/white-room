c3d802de633e7f2e555c71bf0ae30066c3dfd452
Fardin96 <fardinshuvro96@gmail.com>
2024-01-10 22:27:25 +0600

    build uploaded with copy button

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function SignOut() {
 function ChatRoom() {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  // const query = messagesRef.orderBy('createdAt').limit(25);
+  // const query = messagesRef.orderBy('createdAt').limit(50);
   const query = messagesRef.orderBy('createdAt');
 
   const [messages] = useCollectionData(query, { idField: 'id' });
